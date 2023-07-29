9ee1ae891c5fab4837e7bae864035a402b378e79
Jack Henson <jackhenson1@gmail.com>
2023-07-28 20:04:08 -0400

    Added DDL exercise set problems

---
+++ exercises/ddl/exercise_1.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE extrasolar;
+
+CREATE TABLE stars (
+    id serial PRIMARY KEY,
+    name varchar(25) UNIQUE NOT NULL,
+    distance integer NOT NULL CHECK (distance > 0),
+    spectral_type char(1),
+    companions integer NOT NULL CHECK (champions >= 0)
+);
+
+CREATE TABLE planets (
+    id serial PRIMARY KEY,
+    designation char(1) UNIQUE,
+    mass integer
+);
